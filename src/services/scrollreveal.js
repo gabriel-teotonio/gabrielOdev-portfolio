587db75acf80224fd1944a135eb7f9c9238a63dc
Gabriel silva <gabriel23teotonio@gmail.com>
2023-03-11 16:00:53 -0300

    first commit

---
+++ src/services/scrollreveal.js
@@ -0,0 +1,53 @@
+
+ScrollReveal().reveal('.logo',{
+    delay:200,
+});
+
+ScrollReveal().reveal('.headline',{
+    delay:600,
+    origin:'bottom',
+    distance:'100%',    
+});
+
+ScrollReveal().reveal('.icon-list',{
+    delay:1000,
+    origin:'bottom',
+    distance:'100%',    
+});
+
+ScrollReveal().reveal('.img-gabriel',{
+    delay:1000,
+    duration:'1500'
+});
+ScrollReveal().reveal('.btn-main',{
+    delay:1000,
+    duration:'1500',
+    origin:'bottom'
+});
+ScrollReveal().reveal('.title-section',{
+    delay:500,
+    duration:1000,
+    distance:'100%',
+    origin:'bottom',
+});
+
+ScrollReveal().reveal('.tech-list',{
+    delay:600,
+    duration:1000,
+    distance:'120%',
+    origin:'bottom',
+});
+
+ScrollReveal().reveal('.project-item',{
+    delay:500,
+    duration:1000,
+    distance:'150%',
+    origin:'left',
+});
+
+ScrollReveal().reveal('.contact-box',{
+    delay:500,
+    // duration:1000,
+    distance:'100%',
+    origin:'bottom',
+});
