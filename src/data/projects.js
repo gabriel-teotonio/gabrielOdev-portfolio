fcde08c194af0c032112511937f5fdf1f70af89a
Gabriel silva <gabriel23teotonio@gmail.com>
2023-06-04 13:43:48 -0300

    feat: new image in main and responsive layout

---
+++ src/data/projects.js
@@ -0,0 +1,37 @@
+
+const json = {
+    "projects": [
+        {
+            "title": "Landing page Better Call Saul Advocacia",
+            "description": "essa é uma landing page inspirada na série Better Call Saul",
+            "tools": "Javascript, Html e Scss",
+            "linkProject": "https://marley-ph.vercel.app/",
+            "imageUrl": "image.png",
+            "id": 1,
+        },
+        {
+            "title": "Landing page Better Call Saul Advocacia",
+            "description": "essa é uma landing page inspirada na série Better Call Saul",
+            "tools": "Javascript, Html e Scss",
+            "linkProject": "https://marley-ph.vercel.app/",
+            "imageUrl": "image.png",
+            "id": 2,
+        },
+        {
+            "title": "Landing page Better Call Saul Advocacia",
+            "description": "essa é uma landing page inspirada na série Better Call Saul",
+            "tools": "Javascript, Html e Scss",
+            "linkProject": "https://marley-ph.vercel.app/",
+            "imageUrl": "image.png",
+            "id": 3,
+        },
+        {
+            "title": "Landing page Better Call Saul Advocacia",
+            "description": "essa é uma landing page inspirada na série Better Call Saul",
+            "tools": "Javascript, Html e Scss",
+            "linkProject": "https://marley-ph.vercel.app/",
+            "imageUrl": "image.png",
+            "id": 4,
+        },
+    ]
+}
